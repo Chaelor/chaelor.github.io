0a7dd6fb2a001cc16db80db538f597bfecabf5ad
Chaelor <joshsblizzard@gmail.com>
2018-08-03 21:30:18 -0500

    Hid the side nav when a link is clicked on

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 $("document").ready(function () {
-
+    $(".hide-li").on('click', function(){
+        $(".sidenav").hide();
+        $(".sidenav-overlay").hide();
+    })
     //initializes the side navbar
     $('.sidenav').sidenav();
     AOS.init();
